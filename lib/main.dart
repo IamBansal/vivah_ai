52efd9308a157624eeb32f0141a0e015db173b23
IamBansal <abansal11ae@gmail.com>
2024-01-10 22:38:56 +0530

    UI fix

--- lib/main.dart
+++ lib/main.dart
@@ -86,22 +86,48 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Vivah',
-              style: GoogleFonts.carattere(
-                  textStyle: const TextStyle(color: Colors.black, fontSize: 75, fontStyle: FontStyle.italic)
+      body: Stack(
+        children: [
+          //Shader-mask if for adding a black blend behind text
+          ShaderMask(
+            blendMode: BlendMode.srcATop,
+            shaderCallback: (Rect bounds) {
+              return const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.black54, Colors.black45],
+                stops: [0.0, 1],
+              ).createShader(bounds);
+            },
+            child: ClipRRect(
+                child: Image.asset(
+                  'assets/vivah.jpg',
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                )),
+          ),
+          Positioned(
+            left: MediaQuery.of(context).size.width * 0.26,
+            top: MediaQuery.of(context).size.height * 0.4,
+            child:Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Vivah',
+                    style: GoogleFonts.carattere(
+                        textStyle: const TextStyle(color: Colors.white, fontSize: 90, fontStyle: FontStyle.italic)
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 8.0),
+                    child: Text('Your wedding, personalised', style: TextStyle(color: Colors.white ,fontSize: 15 ,fontWeight: FontWeight.bold),),
+                  )
+                ],
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 8.0),
-              child: Text('Your wedding, personalised', style: TextStyle(color: Colors.black ,fontSize: 15),),
-            )
-          ],
-        ),
+            ),),
+        ],
       ),
     );
   }
